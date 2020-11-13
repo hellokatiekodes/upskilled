9b3f7765789419d265dcc008c0b70d6cf6e2eb70
Katie Tran <trankatie8@gmail.com>
2020-11-13 14:46:21 +1100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Simple_Stock_IT_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.dgItems = new System.Windows.Forms.DataGridView();
+            this.ItemCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ItemDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CurrentCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OnOrder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnLogout = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnImport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnImport.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImport.ForeColor = System.Drawing.Color.White;
+            this.btnImport.Location = new System.Drawing.Point(85, 90);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(162, 35);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Open .CSV File";
+            this.btnImport.UseVisualStyleBackColor = false;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Blue;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(65, 382);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(133, 36);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(468, 379);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(150, 39);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // dgItems
+            // 
+            this.dgItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgItems.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ItemCode,
+            this.ItemDescription,
+            this.CurrentCount,
+            this.OnOrder});
+            this.dgItems.Location = new System.Drawing.Point(65, 144);
+            this.dgItems.Name = "dgItems";
+            this.dgItems.RowHeadersWidth = 51;
+            this.dgItems.RowTemplate.Height = 24;
+            this.dgItems.Size = new System.Drawing.Size(553, 205);
+            this.dgItems.TabIndex = 3;
+            this.dgItems.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ItemCode
+            // 
+            this.ItemCode.DataPropertyName = "ItemCode";
+            this.ItemCode.HeaderText = "Item Code";
+            this.ItemCode.MinimumWidth = 6;
+            this.ItemCode.Name = "ItemCode";
+            this.ItemCode.ReadOnly = true;
+            this.ItemCode.Width = 125;
+            // 
+            // ItemDescription
+            // 
+            this.ItemDescription.DataPropertyName = "ItemDescription";
+            this.ItemDescription.HeaderText = "Item Description";
+            this.ItemDescription.MinimumWidth = 6;
+            this.ItemDescription.Name = "ItemDescription";
+            this.ItemDescription.ReadOnly = true;
+            this.ItemDescription.Width = 125;
+            // 
+            // CurrentCount
+            // 
+            this.CurrentCount.DataPropertyName = "CurrentCount";
+            this.CurrentCount.HeaderText = "Current Count";
+            this.CurrentCount.MinimumWidth = 6;
+            this.CurrentCount.Name = "CurrentCount";
+            this.CurrentCount.Width = 125;
+            // 
+            // OnOrder
+            // 
+            this.OnOrder.DataPropertyName = "OnOrder";
+            this.OnOrder.HeaderText = "On Order";
+            this.OnOrder.MinimumWidth = 6;
+            this.OnOrder.Name = "OnOrder";
+            this.OnOrder.ReadOnly = true;
+            this.OnOrder.Width = 125;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(253, 96);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(365, 22);
+            this.txtFile.TabIndex = 4;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnLogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Location = new System.Drawing.Point(468, 45);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(150, 39);
+            this.btnLogout.TabIndex = 5;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 504);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.dgItems);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnImport);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgItems)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.DataGridView dgItems;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ItemCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ItemDescription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CurrentCount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OnOrder;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.DAL;
+
+namespace Simple_Stock_IT_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.ShowDialog();
+                int ImportedRecord = 0, inValidItem = 0;
+                string SourceURl = "";
+
+                if (dialog.FileName != "")
+                {
+                    if (dialog.FileName.EndsWith(".csv"))
+                    {
+
+                        
+
+
+                        DataTable dtNew = new DataTable();
+                        DataGrid dgItems = new DataGrid();
+
+                        dtNew = GetDataTabletFromCSVFile(dialog.FileName);
+                        if (Convert.ToString(dtNew.Columns[0]).ToLower() != "lookupcode")
+                        {
+                            MessageBox.Show("Invalid Items File");
+                            btnSave.Enabled = false;
+                            return;
+                        }
+                        txtFile.Text = dialog.SafeFileName;
+                        SourceURl = dialog.FileName;
+                        if (dtNew.Rows != null && dtNew.Rows.ToString() != String.Empty)
+                        {
+                            dgItems.DataSource = dtNew;
+                        }
+                        foreach (DataGridViewRow row in dgItems.Rows)
+                        {
+                            if (Convert.ToString(row.Cells["ItemCode"].Value) == "" || row.Cells["ItemCode"].Value == null
+                                || Convert.ToString(row.Cells["ItemDescription"].Value) == "" || row.Cells["ItemDescription"].Value == null
+                                || Convert.ToString(row.Cells["CurrentCount"].Value) == "" || row.Cells["CurrentCount"].Value == null
+                                || Convert.ToString(row.Cells["OnOrder"].Value) == "" || row.Cells["OnOrder"].Value == null)
+                            {
+                                row.DefaultCellStyle.BackColor = Color.Red;
+                                inValidItem += 1;
+                            }
+                            else
+                            {
+                                ImportedRecord += 1;
+                            }
+                        }
+                        if (dgItems.Rows.Count == 0)
+                        {
+                            btnSave.Enabled = false;
+                            MessageBox.Show("There is no data in this file", "GAUTAM POS", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Selected File is Invalid, Please Select valid csv file.", "GAUTAM POS", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex);
+            }
+        }
+
+        //to create GetDataTabletfromCSVFile function 
+
+
+        public static DataTable GetDataTabletFromCSVFile(string csv_file_path)
+        {
+            DataTable csvData = new DataTable();
+            try
+            {
+                if (csv_file_path.EndsWith(".csv"))
+                {
+                    using (Microsoft.VisualBasic.FileIO.TextFieldParser csvReader = new Microsoft.VisualBasic.FileIO.TextFieldParser(csv_file_path))
+                    {
+                        csvReader.SetDelimiters(new string[] { "," });
+                        csvReader.HasFieldsEnclosedInQuotes = true;
+                        //we are reading the column
+                        string[] colFields = csvReader.ReadFields();
+                        foreach (string column in colFields)
+                        {
+                            DataColumn datecolumn = new DataColumn(column);
+                            datecolumn.AllowDBNull = true;
+                            csvData.Columns.Add(datecolumn);
+                        }
+                        while (!csvReader.EndOfData)
+                        {
+                            string[] fieldData = csvReader.ReadFields();
+                            for (int i = 0; i < fieldData.Length; i++)
+                            {
+                                if (fieldData[i] == "")
+                                {
+                                    fieldData[i] = null;
+                                }
+                            }
+                            csvData.Rows.Add(fieldData);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exce " + ex);
+            }
+            return csvData;
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dtItem = (DataTable)(dgItems.DataSource);
+                string Lookup, description, dept, UnitPrice;
+                string InsertItemQry = "";
+                int count = 0;
+                foreach (DataRow dr in dtItem.Rows)
+                {
+                    Lookup = Convert.ToString(dr["LookupCode"]);
+                    description = Convert.ToString(dr["ItemName"]);
+                    dept = Convert.ToString(dr["DeptId"]);
+                    UnitPrice = Convert.ToString(dr["Price"]);
+                    if (Lookup != "" && description != "" && dept != "" && UnitPrice != "")
+                    {
+                        InsertItemQry += "Insert into tbItem(LookupCode,ItemName,DeptId,CateId,Cost,Price, Quantity, UOM, Weight, TaxID, IsDiscountItem,EntryDate)Values('" + Lookup + "','" + description + "','" + dept + "','" + dr["CateId"] + "','" + dr["Cost"] + "','" + UnitPrice + "'," + dr["Quantity"] + ",'" + dr["UOM"] + "','" + dr["Weight"] + "','" + dr["TaxID"] + "','" + dr["IsDiscountItem"] + "',GETDATE()); ";
+                        count++;
+                    }
+                }
+                if (InsertItemQry.Length > 5)
+                {
+                    bool isSuccess = DBAccess.ExecuteQuery(InsertItemQry);
+                    if (isSuccess)
+                    {
+                        MessageBox.Show("Item Imported Successfully, Total Imported Records : " + count + "", "GAUTAM POS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        dgItems.DataSource = null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex);
+            }
+        }
+    }
+}
+
